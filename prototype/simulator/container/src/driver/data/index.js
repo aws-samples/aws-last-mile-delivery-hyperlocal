20833765359c5d93b7366b69d8e0d6aff31f0195
mirgj <mirash.gjolaj@gmail.com>
2022-02-15 13:02:05 +0800

    feat(sim): add support for multiple orders per driver. see #14

--- prototype/simulator/container/src/driver/data/index.js
+++ prototype/simulator/container/src/driver/data/index.js
@@ -21,96 +21,82 @@ const helper = require('../../common/helper')
 
 let startingPosition
 let currentPosition
-let originPath
-let restProgress = 0
-let destinationPath
-let custProgress = 0
+let currentSegment
+let currentProgress
+let currentPath
+
+const getIdlePosition = (lat, long, radius) => {
+	if (!startingPosition) {
+		logger.info('Generating starting point...')
+		startingPosition = helper.generateRandomPoint(lat, long, radius)
+
+		return [startingPosition.latitude, startingPosition.longitude, startingPosition.elevation]
+	}
+
+	const { latitude, longitude } = startingPosition
+	// after the first initialization move the driver around the starting position
+	// within 100 meters to simulate motion
+	currentPosition = helper.generateRandomPoint(latitude, longitude, 100)
+
+	return [currentPosition.latitude, currentPosition.longitude, currentPosition.elevation]
+}
+
+const getSegmentLocations = (lat, long, radius, status, segments) => {
+	if (currentSegment >= segments.length) {
+		logger.info('Played all segments for assigment.')
+		state.setState('assignmentStatus', state.ASSIGNMENT_STATUS.IDLE)
+
+		return getIdlePosition(lat, long, radius)
+	}
+
+	const { orderId, from, to, segmentType, route } = segments[currentSegment]
+
+	state.setState('orderId', orderId)
+
+	if (!currentPath) {
+		currentPath = polyline.decode(route.points)
+
+		logger.info('Following path on: ', JSON.stringify(currentPath))
+	}
+	const [_lat, _long] = currentPath[currentProgress]
+
+	if (currentProgress < currentPath.length - 1) {
+		currentProgress++
+
+		return [_lat, _long, 0]
+	} else {
+		if (segmentType === 'TO_ORIGIN' && status === state.STATUSES.PICKING_UP_GOODS) {
+			logger.info('Arrived at origin. Order: ', orderId)
+
+			state.setState('status', state.STATUSES.ARRIVED_AT_ORIGIN)
+		}
+
+		if (segmentType === 'TO_DESTINATION' && status === state.STATUSES.DELIVERING) {
+			logger.info('Arrived at destination. Order: ', orderId)
+
+			state.setState('status', state.STATUSES.ARRIVED_AT_DESTINATION)
+		}
+
+		currentPath = null
+		currentProgress = 0
+		currentSegment++
+
+		return getSegmentLocations(lat, long, status, segments)
+	}
+}
 
 const getCoordinates = (lat, long, radius, status) => {
-	switch (status) {
-		case state.STATUSES.PICKING_UP_GOODS:
-		case state.STATUSES.ARRIVED_AT_ORIGIN: {
-			const toorigin = state.getState().routing
+	if (state.getState().assignmentStatus === state.ASSIGNMENT_STATUS.IDLE) {
+		currentPath = null
+		currentProgress = 0
+		currentSegment = 0
 
-			if (toorigin.length > 0) {
-				if (!originPath) {
-					const pathPolyline = toorigin[0].pathPolyline
-					originPath = polyline.decode(pathPolyline)
+		state.setState('segments', state.DEFAULT_STATE.segments)
 
-					logger.info('origin path')
-					logger.info(originPath)
-				}
-
-				const [_lat, _long] = originPath[restProgress]
-
-				if (restProgress === originPath.length - 1) {
-					if (status === state.STATUSES.PICKING_UP_GOODS) {
-						state.setState('status', state.STATUSES.ARRIVED_AT_ORIGIN)
-					}
-				} else {
-					restProgress++
-				}
-
-				return [_lat, _long, 0]
-			}
-
-			return [lat, long, 0]
-		}
-		case state.STATUSES.DELIVERING:
-		case state.STATUSES.ARRIVED_AT_DESTINATION:
-		case state.STATUSES.DELIVERED: {
-			const todestination = state.getState().routing
-
-			if (todestination.length > 1) {
-				if (!destinationPath) {
-					const pathPolyline = todestination[1].pathPolyline
-					destinationPath = polyline.decode(pathPolyline)
-
-					logger.info('destination path')
-					logger.info(destinationPath)
-				}
-
-				const [_lat, _long] = destinationPath[custProgress]
-
-				if (custProgress === destinationPath.length - 1) {
-					if (status === state.STATUSES.DELIVERING) {
-						state.setState('status', state.STATUSES.ARRIVED_AT_DESTINATION)
-					}
-				} else {
-					custProgress++
-				}
-
-				return [_lat, _long, 0]
-			}
-
-			return [lat, long, 0]
-		}
-		case state.STATUSES.IDLE:
-		case state.STATUSES.ACCEPTED:
-		default: {
-			destinationPath = null
-			originPath = null
-			custProgress = 0
-			restProgress = 0
-
-			state.setState('routing', state.DEFAULT_STATE.routing)
-			state.setState('destination', state.DEFAULT_STATE.destination)
-			state.setState('origin', state.DEFAULT_STATE.origin)
-
-			if (!startingPosition) {
-				startingPosition = helper.generateRandomPoint(lat, long, radius)
-
-				return [startingPosition.latitude, startingPosition.longitude, startingPosition.elevation]
-			}
-
-			const { latitude, longitude } = startingPosition
-			// after the first initialization move the driver around the starting position
-			// within 100 meters to simulate motion
-			currentPosition = helper.generateRandomPoint(latitude, longitude, 100)
-
-			return [currentPosition.latitude, currentPosition.longitude, currentPosition.elevation]
-		}
+		return getIdlePosition(lat, long, radius)
 	}
+
+	return getSegmentLocations(lat, long, radius, status, state.getState().segments)
 }
 
 /// range not used anymore at this point, due to wrong random point generation
