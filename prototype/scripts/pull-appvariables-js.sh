10d4e738807163f3a3086e2929f72ce1914e03f6
Greg Varga <gergelyv@amazon.com>
2022-02-17 12:10:23 +0800

    chore(scripts): use .env to store vars to pull appVariables

--- prototype/scripts/pull-appvariables-js.sh
+++ prototype/scripts/pull-appvariables-js.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-AWS_ACCOUNT_ID=XXXXXXXXXXXX
+if [ -f "./.env" ]; then
+    . ./.env
+else
+    PROFILE=hyperlocalAdmin
+    NAMESPACE=sim
+    REGION=ap-southeast-1
+fi
 
-SIMULATOR_NAMESPACE=sim
-BUCKET=$SIMULATOR_NAMESPACE-simulator-website-$AWS_ACCOUNT_ID-ap-southeast-1
-PROFILE=hyperlocalAdmin
-REGION=ap-southeast-1
+ACCOUNT_ID=$(aws sts get-caller-identity --profile $PROFILE --region $REGION --output json | jq .Account --raw-output)
+BUCKET=$NAMESPACE-simulator-website-$ACCOUNT_ID-$REGION
 
 aws s3 cp s3://${BUCKET}/static/appVariables.js ../simulator/website/public/static/ --profile $PROFILE --region $REGION
