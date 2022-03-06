b28dc7722b15a9e589344febb898dcb66acd882f
Greg Varga <gergelyv@amazon.com>
2022-03-06 21:36:46 +0800

    fix(dispatcher): repair quarkus build errors

--- prototype/dispatch/delivery-dispatch/apps/app-core/src/main/java/dev/aws/proto/apps/appcore/api/DispatchService.java
+++ prototype/dispatch/delivery-dispatch/apps/app-core/src/main/java/dev/aws/proto/apps/appcore/api/DispatchService.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package dev.aws.proto.apps.appcore.api;
 
 import dev.aws.proto.apps.appcore.api.request.DispatchRequest;
@@ -5,9 +22,8 @@ import dev.aws.proto.apps.appcore.config.SolutionConfig;
 import dev.aws.proto.apps.appcore.planner.solution.DispatchSolutionBase;
 import dev.aws.proto.apps.appcore.planner.solution.SolutionState;
 import dev.aws.proto.core.Order;
-import dev.aws.proto.core.routing.route.GraphhopperRouter;
 import dev.aws.proto.core.routing.config.RoutingConfig;
-import org.optaplanner.core.api.score.buildin.hardmediumsoftlong.HardMediumSoftLongScore;
+import dev.aws.proto.core.routing.route.GraphhopperRouter;
 import org.optaplanner.core.api.solver.SolverManager;
 import org.optaplanner.core.config.solver.SolverConfig;
 import org.optaplanner.core.config.solver.SolverManagerConfig;
@@ -29,8 +45,14 @@ public abstract class DispatchService<
     protected RoutingConfig routingConfig;
     protected SolutionConfig solutionConfig;
     protected GraphhopperRouter graphhopperRouter;
-    protected SolverManager<TDispatchSolution, UUID> solverManager;
+    protected final SolverManager<TDispatchSolution, UUID> solverManager;
     protected final Map<UUID, SolutionState<TDispatchSolution, UUID>> solutionMap;
+
+    protected DispatchService() {
+        SolverConfig solverConfig = SolverConfig.createFromXmlFile(java.nio.file.Path.of(this.solutionConfig.getSolverConfigXmlPath()).toFile());
+        this.solverManager = SolverManager.create(solverConfig, new SolverManagerConfig());
+        this.solutionMap = new ConcurrentHashMap<>();
+    }
 
     protected DispatchService(RoutingConfig routingConfig, SolutionConfig solutionConfig) {
         this.routingConfig = routingConfig;
@@ -45,6 +67,7 @@ public abstract class DispatchService<
     }
 
     protected abstract void solveDispatchProblem(UUID problemId, TDispatchRequest request);
+
     protected abstract void finalBestSolutionConsumer(TDispatchSolution solution);
 
     protected TDispatchSolution problemFinder(UUID problemId) {
--- prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/api/DispatchService.java
+++ prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/api/DispatchService.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package dev.aws.proto.apps.instant.sequential.api;
 
 import dev.aws.proto.apps.appcore.api.response.RequestResult;
@@ -50,7 +67,6 @@ public class DispatchService extends dev.aws.proto.apps.appcore.api.DispatchServ
 
     @Inject
     DdbAssignmentService assignmentService;
-
 
     DispatchService(RoutingConfig routingConfig, SolutionConfig solutionConfig) {
         super(routingConfig, solutionConfig);
@@ -154,7 +170,6 @@ public class DispatchService extends dev.aws.proto.apps.appcore.api.DispatchServ
         logger.debug("Removing problemId {} from solutionMap at consumeSolution", problemId);
         this.solutionMap.remove(problemId);
     }
-
 
 
     private void consumeSolution(DispatchSolution solution) {
