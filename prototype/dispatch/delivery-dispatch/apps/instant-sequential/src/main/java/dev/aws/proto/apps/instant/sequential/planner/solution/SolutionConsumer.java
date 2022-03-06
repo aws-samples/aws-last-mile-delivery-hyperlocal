5fb25552a4c40df1ed770515ca868958aeb84fb6
Greg Varga <gergelyv@amazon.com>
2022-03-06 21:25:16 +0800

    refactor(dispatcher): update copyright headers

--- prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/planner/solution/SolutionConsumer.java
+++ prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/planner/solution/SolutionConsumer.java
@@ -1,27 +1,18 @@
-/*-
- * ========================LICENSE_START=================================
- * Order Dispatcher
- * %%
- * Copyright (C) 2006 - 2022 Amazon Web Services
- * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * =========================LICENSE_END==================================
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package dev.aws.proto.apps.instant.sequential.planner.solution;
 
@@ -133,7 +124,7 @@ public class SolutionConsumer {
                     assignedOrderSegments.add(
                             DeliverySegment.builder()
                                     .orderId(delivery.getOrder().getOrderId())
-                                    .index(segmentCtr+1)
+                                    .index(segmentCtr + 1)
                                     .from(delivery.getPickup().coordinate())
                                     .to(delivery.getDropoff().coordinate())
                                     .segmentType(DeliverySegment.SegmentType.TO_DESTINATION)
