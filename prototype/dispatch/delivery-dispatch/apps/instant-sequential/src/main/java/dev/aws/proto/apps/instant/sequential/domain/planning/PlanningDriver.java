5cca9b194b8c8e01409a9ca792590bfd833d5cbb
Greg Varga <gergelyv@amazon.com>
2022-03-06 21:36:46 +0800

    fix(dispatcher): repair quarkus build errors

--- prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/domain/planning/PlanningDriver.java
+++ prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/domain/planning/PlanningDriver.java
@@ -1,40 +1,35 @@
-/*-
- * ========================LICENSE_START=================================
- * Order Dispatcher
- * %%
- * Copyright (C) 2006 - 2022 Amazon Web Services
- * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * =========================LICENSE_END==================================
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package dev.aws.proto.apps.instant.sequential.domain.planning;
 
 import dev.aws.proto.apps.instant.sequential.location.DriverLocation;
 import dev.aws.proto.core.routing.distance.Distance;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.optaplanner.core.api.domain.lookup.PlanningId;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class PlanningDriver implements DeliveryOrDriver {
-    private final String id;
+    private String id;
     private String driverIdentity;
     private DriverLocation location;
     private String status;
@@ -68,7 +63,7 @@ public class PlanningDriver implements DeliveryOrDriver {
     }
 
     public String getShortId() {
-        return this.id.substring(0,8);
+        return this.id.substring(0, 8);
     }
 
     public Distance getDistanceFromPickup(PlanningDelivery delivery) {
--- prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/planner/solution/DispatchSolution.java
+++ prototype/dispatch/delivery-dispatch/apps/instant-sequential/src/main/java/dev/aws/proto/apps/instant/sequential/planner/solution/DispatchSolution.java
@@ -1,11 +1,30 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package dev.aws.proto.apps.instant.sequential.planner.solution;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.aws.proto.apps.appcore.planner.solution.DispatchSolutionBase;
 import dev.aws.proto.apps.instant.sequential.domain.planning.PlanningDelivery;
 import dev.aws.proto.apps.instant.sequential.domain.planning.PlanningDriver;
 import dev.aws.proto.apps.instant.sequential.util.Constants;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
 import org.optaplanner.core.api.domain.solution.PlanningScore;
 import org.optaplanner.core.api.domain.solution.PlanningSolution;
@@ -18,9 +37,15 @@ import java.util.UUID;
 
 @PlanningSolution
 @Data
+@NoArgsConstructor
 public class DispatchSolution extends DispatchSolutionBase<HardMediumSoftLongScore> {
     private List<PlanningDriver> planningDrivers;
     private List<PlanningDelivery> planningDeliveries;
+
+    @PlanningScore
+    @Getter
+    @Setter
+    private HardMediumSoftLongScore score;
 
     public DispatchSolution(UUID id, String name, long createdAt, String executionId, List<PlanningDriver> planningDrivers, List<PlanningDelivery> planningDeliveries) {
         this.id = id;
@@ -41,11 +66,5 @@ public class DispatchSolution extends DispatchSolutionBase<HardMediumSoftLongSco
     @ValueRangeProvider(id = Constants.PlanningDeliveryRange)
     public List<PlanningDelivery> getPlanningDeliveries() {
         return planningDeliveries;
-    }
-
-    @PlanningScore
-    @Override
-    public HardMediumSoftLongScore getScore() {
-        return score;
     }
 }
