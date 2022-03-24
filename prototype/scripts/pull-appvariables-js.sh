aad39975a5f4b7a9889dc0a138b03789c551af0b
Greg Varga <gergelyv@amazon.com>
2022-03-24 19:51:16 +0800

    feat(scripts): add reset sdd docker helper script

--- prototype/scripts/pull-appvariables-js.sh
+++ prototype/scripts/pull-appvariables-js.sh
@@ -4,11 +4,11 @@ if [ -f "./.env" ]; then
     . ./.env
 else
     PROFILE=hyperlocalAdmin
-    NAMESPACE=sim
+    NAMESPACE_SIM=sim
     REGION=ap-southeast-1
 fi
 
 ACCOUNT_ID=$(aws sts get-caller-identity --profile $PROFILE --region $REGION --output json | jq .Account --raw-output)
-BUCKET=$NAMESPACE-simulator-website-$ACCOUNT_ID-$REGION
+BUCKET=$NAMESPACE_SIM-simulator-website-$ACCOUNT_ID-$REGION
 
 aws s3 cp s3://${BUCKET}/static/appVariables.js ../simulator/website/public/static/ --profile $PROFILE --region $REGION
