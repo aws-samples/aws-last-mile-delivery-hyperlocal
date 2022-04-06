0073f6389b8c75e603039d7e9d7c9911ac6f4e21
Greg Varga <gergelyv@amazon.com>
2022-04-06 10:17:37 +0700

    feat(delivery-dispatch/apps/sameday): add capacity concept

---
+++ prototype/dispatch/delivery-dispatch/apps/sameday-directpudo/src/main/java/dev/aws/proto/apps/sameday/directpudo/domain/planning/capacity/CurrentCapacity.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dev.aws.proto.apps.sameday.directpudo.domain.planning.capacity;
+
+import dev.aws.proto.apps.sameday.directpudo.data.Parcel;
+import dev.aws.proto.apps.sameday.directpudo.exception.CapacityException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class CurrentCapacity extends CapacityBase {
+    private MaxCapacity maxCapacity;
+
+    public boolean canAddParcel(Parcel parcel) {
+        float newLength = Math.max(this.getLength(), parcel.getLength());
+        float newHeight = this.getHeight() + parcel.getHeight();
+        float newWidth = Math.max(this.getWidth(), parcel.getWidth());
+        float newWeight = this.getWeight() + parcel.getWeight();
+
+        return !this.maxCapacity.exceeds(newLength, newHeight, newWidth, newWeight);
+    }
+
+    public void addParcel(Parcel parcel) {
+        if (!this.canAddParcel(parcel)) {
+            throw new CapacityException(String.format("Cannot add parcel %s, it exceeds max capacity %s", parcel, maxCapacity));
+        }
+
+        this.setLength(Math.max(this.getLength(), parcel.getLength()));
+        this.setHeight(this.getHeight() + parcel.getHeight());
+        this.setWidth(Math.max(this.getWidth(), parcel.getWidth()));
+        this.setWeight(this.getWeight() + parcel.getWeight());
+    }
+
+    public void removeParcel(Parcel parcel) {
+        // don't change length/width as we don't have enough information about
+        // previous maxLengths/maxWidths, unless we maintain it in a linked list
+        // but it's not necessary ATM
+        this.setHeight(Math.max(this.getHeight() - parcel.getHeight(), 0));
+        this.setWeight(Math.max(this.getWeight() - parcel.getWeight(), 0));
+    }
+
+}
--- prototype/dispatch/delivery-dispatch/apps/sameday-directpudo/src/main/java/dev/aws/proto/apps/sameday/directpudo/domain/planning/capacity/MaxCapacity.java
+++ prototype/dispatch/delivery-dispatch/apps/sameday-directpudo/src/main/java/dev/aws/proto/apps/sameday/directpudo/domain/planning/capacity/MaxCapacity.java
@@ -15,21 +15,17 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package dev.aws.proto.apps.sameday.directpudo.data;
+package dev.aws.proto.apps.sameday.directpudo.domain.planning.capacity;
 
-import dev.aws.proto.apps.sameday.directpudo.domain.planning.capacity.CapacityBase;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-/**
- * Represents a parcel in the sameday-directpudo delivery domain.
- */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @SuperBuilder
-public class Parcel extends CapacityBase {
-    private String type;
+public class MaxCapacity extends CapacityBase {
+    public boolean exceeds(float length, float height, float width, float weight) {
+        return
+                length > this.getLength()
+                        || height > this.getHeight()
+                        || width > this.getWidth()
+                        || weight > this.getWidth();
+    }
 }
